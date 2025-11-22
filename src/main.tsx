d0a8f87bb719d61c15c834f143e900c1abc38c14
Colm Digby <cdigby@iris-eng.com>
2025-11-22 22:44:56 +0100

    bug fix2

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import './i18n'; // Import i18n configuration
@@ -6,8 +5,8 @@ import App from './App';
 import { Toaster } from 'sonner';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <>
     <App />
     <Toaster position="top-right" />
-  </StrictMode>,
+  </>,
 );
